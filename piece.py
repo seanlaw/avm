2729e3477d25efa85c808ba0760b9aeef1ea9b1f
Sean Law <seanmylaw@gmail.com>
2019-01-02 20:05:49 -0500

    Added generation of all ref rotations & indices

--- piece.py
+++ piece.py
@@ -4,18 +4,25 @@ import numpy as np
 from textwrap import dedent
 
 class PIECE(object):
-    def __init__(self, ref_idx=None, name=None, shape=None):
+    def __init__(self, ref_idx=None, ref_arr=None, name=None, shape=None):
         self._ref_idx = ref_idx
+        self._ref_arr = ref_arr
         self._name = name
         self._shape = shape
 
     @property
     def ref_idx(self):
+        """
+        Return a list of reference array indices
+        """
         return self._ref_idx
 
-    @ref_idx.setter
-    def ref_idx(self, value):
-        self._ref_idx = value
+    @property
+    def ref_arr(self):
+        """
+        Return a list of reference arrays
+        """
+        return self._ref_arr
 
     @property
     def name(self):
@@ -25,11 +32,32 @@ class PIECE(object):
     def shape(self):
         return self._shape
 
+    @staticmethod
+    def get_ref_rots(start_idx, n_rotations):
+        tmp_arr = np.zeros((start_idx.max()+1, start_idx.max()+1), dtype='u1')
+        tmp_arr[tuple(start_idx.T)] = 1
+        ref_idx = []
+        ref_arr = []
+        for i in range(n_rotations):
+            rot_arr = np.rot90(tmp_arr, -i)
+            # Shift array upward if necessary
+            while(not np.any(rot_arr[0, :])):
+                rot_arr = np.roll(rot_arr, -1, axis=0)
+            # Shift array leftward if necessary
+            while(not np.any(rot_arr[:, 0])):
+                rot_arr = np.roll(rot_arr, -1, axis=1)
+            ref_arr.append(rot_arr)
+            ref_idx.append(np.argwhere(rot_arr > 0))
+
+        return ref_idx, ref_arr
+
 class RED(PIECE):
     def __init__(self):
         shape = "o-o"
-        ref_idx = np.array([[0,0], [0,1]])
-        super().__init__(ref_idx=ref_idx, name='A', shape=shape)
+        start_idx = np.array([[0,0], [0,1]])
+        ref_idx, ref_arr = self.get_ref_rots(start_idx, 1)  # Don't rotate!
+
+        super().__init__(ref_idx=ref_idx, ref_arr=ref_arr, name='A', shape=shape)
 
 class ORANGE(PIECE):
     def __init__(self):
@@ -38,8 +66,10 @@ class ORANGE(PIECE):
                         / \ 
                        o   o
                        """)
-        ref_idx = np.array([[1,0], [0,1], [1,2]])
-        super().__init__(ref_idx=ref_idx, name='F', shape=shape)
+        start_idx = np.array([[1,0], [0,1], [1,2]])
+        ref_idx, ref_arr = self.get_ref_rots(start_idx, 4)
+
+        super().__init__(ref_idx=ref_idx, ref_arr=ref_arr, name='F', shape=shape)
 
 class YELLOW(PIECE):
     
@@ -49,8 +79,10 @@ class YELLOW(PIECE):
                         \ 
                          o
                        """)
-        ref_idx = np.array([[0,0], [1,1]])
-        super().__init__(ref_idx=ref_idx, name='B', shape=shape)
+        start_idx = np.array([[0,0], [1,1]])
+        ref_idx, ref_arr = self.get_ref_rots(start_idx, 2)  # Only rotate once!
+
+        super().__init__(ref_idx=ref_idx, ref_arr=ref_arr, name='B', shape=shape)
 
 class GREEN(PIECE):
     def __init__(self):
@@ -59,8 +91,10 @@ class GREEN(PIECE):
                           \ 
                            o
                        """)
-        ref_idx = np.array([[0,0], [0,1], [1,2]])
-        super().__init__(ref_idx=ref_idx, name='C', shape=shape)
+        start_idx = np.array([[0,0], [0,1], [1,2]])
+        ref_idx, ref_arr = self.get_ref_rots(start_idx, 4)
+
+        super().__init__(ref_idx=ref_idx, ref_arr=ref_arr, name='C', shape=shape)
 
 class BLUE(PIECE):
     def __init__(self):
@@ -69,8 +103,10 @@ class BLUE(PIECE):
                          |
                        o-o
                        """)
-        ref_idx = np.array([[0,1], [1,0], [1,1]])
-        super().__init__(ref_idx=ref_idx, name='D', shape=shape)
+        start_idx = np.array([[0,1], [1,0], [1,1]])
+        ref_idx, ref_arr = self.get_ref_rots(start_idx, 4)
+
+        super().__init__(ref_idx=ref_idx, ref_arr=ref_arr, name='D', shape=shape)
 
 class PURPLE(PIECE):
     def __init__(self):
@@ -79,17 +115,28 @@ class PURPLE(PIECE):
                         / \ 
                        o   o
                        """)
-        ref_idx = np.array([[0,1], [1,0], [1,2]])
-        super().__init__(ref_idx=ref_idx, name='E', shape=shape)
+        start_idx = np.array([[0,1], [1,0], [1,2]])
+        ref_idx, ref_arr = self.get_ref_rots(start_idx, 4)
+
+        super().__init__(ref_idx=ref_idx, ref_arr=ref_arr, name='E', shape=shape)
 
 class WHITE(PIECE):
     def __init__(self):
         shape = "o"
         ref_idx = np.array([[0,0]])
-        super().__init__(ref_idx=ref_idx, name='E', shape=shape)
+        ref_idx, ref_arr = self.get_ref_rots(start_idx, 0)
+
+        super().__init__(ref_idx=ref_idx, ref_arr=ref_arr, name='E', shape=shape)
 
 if __name__ == '__main__':
-    red = RED()
-    print(red.ref_idx)
-    #orange = ORANGE()
-    #print(orange.shape)
+    #red = RED()
+    #print(red.ref_arr)
+    orange = ORANGE()
+    for arr in orange.ref_arr:
+        print(arr)
+    #yellow = YELLOW()
+    #print(yellow.ref_arr)
+    #green = GREEN()
+    #print(green.ref_arr)
+    #blue = BLUE()
+    #print(blue.ref_arr)
