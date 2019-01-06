940f016f5ead196e9884807751e6c998e47802fb
Sean Law <seanmylaw@gmail.com>
2019-01-06 01:08:45 -0500

    Freezing numpy algo x. Can't generalize. Need dancing links

--- algox.py
+++ algox.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.sparse import csc_matrix
 from collections import deque
 
-def search(x, ref_rows, partials=None, solutions=None, primary_idx=None):
+def _search(x, ref_rows, partials=None, solutions=None, level=0):
     """
     """
     if partials is None:
@@ -13,11 +13,10 @@ def search(x, ref_rows, partials=None, solutions=None, primary_idx=None):
     if solutions is None:
         solutions = []
 
-
     rows = np.array(range(x.shape[0]))
     cols = np.array(range(x.shape[1]))
 
-    if len(cols) == 0:  
+    if len(cols) == 0:
         if len(partials) == 0:
             print("Your matrix is empty!")
         else:
@@ -25,27 +24,37 @@ def search(x, ref_rows, partials=None, solutions=None, primary_idx=None):
         return
     
     col_sum = np.sum(x, axis=0)
+    c = np.argmin(col_sum)
     if np.min(col_sum) == 0:  # no solution
-        partials.pop()
+        if len(partials) > 0:
+            partials.pop()
         return
 
-    c = np.argmin(col_sum)
     for r in x[:, c].nonzero()[0]:
-
+    
         partials.append(ref_rows[r])  # r is included in partial solution
         delete_cols = x[r, :].nonzero()[1]
-        delete_rows = np.unique(x[:, delete_cols].nonzero()[0])        
+        delete_rows = np.unique(x[:, delete_cols].nonzero()[0])
         keep_rows = np.delete(rows, delete_rows)
         keep_cols = np.delete(cols, delete_cols)
         
         new_ref_rows = dict(list(enumerate([ref_rows[k] for k in keep_rows])))
         
-        search(x[keep_rows][:, keep_cols], new_ref_rows, partials, solutions)
+        _search(x[keep_rows][:, keep_cols], 
+               new_ref_rows, 
+               partials, 
+               solutions,
+               level=level+1,
+              )
         
         if len(partials) > 0:
             partials.pop()
     
     return solutions
+
+def search(x):
+
+    return
 
 if __name__ == '__main__':
 
@@ -61,7 +70,7 @@ if __name__ == '__main__':
     csc = csc_matrix(arr)
     ref_rows = dict(list(enumerate(range(csc.shape[0]))))
 
-    print(search(csc, ref_rows))
+    print(_search(csc, ref_rows))
 
     # Exact Cover Example #2
     arr = np.array([[1, 0, 0, 1, 0, 0, 1],
@@ -81,9 +90,30 @@ if __name__ == '__main__':
                 5: 'F',
                }
 
-    print(search(csc, ref_rows))
+    print(_search(csc, ref_rows))
 
     # Generalized Cover Example #1
+    # 2x2 grid with one L-shaped and two Singleton-shaped pieces.
+    #                0  1  2  3  A  B  C
+    arr = np.array([[1, 0, 1, 1, 1, 0, 0],
+                    [1, 0, 0, 0, 0, 1, 0],
+                    [0, 1, 0, 0, 0, 1, 0],
+                    [0, 0, 1, 0, 0, 1, 0],
+                    [0, 0, 0, 1, 0, 1, 0],
+                    [1, 0, 0, 0, 0, 0, 1],
+                    [0, 1, 0, 0, 0, 0, 1],
+                    [0, 0, 1, 0, 0, 0, 1],
+                    [0, 0, 0, 1, 0, 0, 1],
+                   ], dtype='u1')
+
+    csc = csc_matrix(arr)
+    ref_rows = dict(list(enumerate(range(csc.shape[0]))))
+
+    #print(search(csc, ref_rows, primary_idx=[0,2,3]))
+    #print(search(csc, ref_rows, primary_idx=[0,1,2,3]))
+    #print(search(csc, ref_rows, primary_idx=[0]))
+
+    # Generalized Cover Example #2
     # 3x3 grid with one L-shaped, one (2x2) Square-shaped, and one 
     # Singleton-shaped piece.
     # Columns 0-8 (inclusive) represent the board and are secondary.
@@ -112,4 +142,4 @@ if __name__ == '__main__':
     csc = csc_matrix(arr)
     ref_rows = dict(list(enumerate(range(csc.shape[0]))))
 
-    print(search(csc, ref_rows))
+    #print(search(csc, ref_rows, primary_idx=[9]))
