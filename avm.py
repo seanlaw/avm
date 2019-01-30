793ce2bae82c3b984f6f81b80101ff16c30ea4cb
Sean Law <seanmylaw@gmail.com>
2019-01-29 21:22:31 -0500

    Added logging to avm.py

--- avm.py
+++ avm.py
@@ -3,6 +3,9 @@
 import game
 from dxz import DXZ
 from scipy.sparse import csc_matrix
+import logging
+
+logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     g = game.GAME()
@@ -11,5 +14,7 @@ if __name__ == '__main__':
     csc = csc_matrix(g.pieces_pos, dtype='u1')
 
     dxz = DXZ(csc)
-    dxz.search(log_time=True, log_resources=True, every=60.0)
+    #dxz.search(log_time=True, log_resources=True, every=60.0)
+    dxz.search(log_time=True, log_resources=False)
+    logger.warning(len(dxz))
     #dxz.print_solutions()
